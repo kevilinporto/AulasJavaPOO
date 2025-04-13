3e474dfb79feb8f95dbe477c35ffae607c976d8b
Kevilin Porto <kevilinvitoria22@gmail.com>
2025-04-12 23:14:33 -0300

    Exercícios abstração

--- src/main/java/com/senai/aula06_abstracao/exercicios/sistema_check_in/Main.java
+++ src/main/java/com/senai/aula06_abstracao/exercicios/sistema_check_in/Main.java
@@ -12,8 +12,8 @@ public class Main {
         int opcao;
         do {
             System.out.println("""
-                    |       Selecione uma opção:              |
-                    |                                         |
+                    |       Selecione uma opção:             |
+                    |                                        |
                     | 1 - Validar documentos                 |
                     | 2 - Emitir cartão de embarque          |
                     | 3 - Verificar peso da bagagem          |
@@ -23,11 +23,25 @@ public class Main {
             scanner.nextLine();
 
             switch (opcao) {
-                case 1 -> companhia.validarDocumentos();
-                case 2 -> companhia.emitirCartaoEmbarque();
-                case 3 -> companhia.checarPesoBagagem(scanner);
-                case 4 -> System.out.println("Encerrando o programa...");
-                default -> System.out.println("Opção inválida.");
+                case 1:
+                    companhia.validarDocumentos();
+                break;
+
+                case 2:
+                    companhia.emitirCartaoEmbarque();
+                break;
+
+                case 3:
+                    companhia.checarPesoBagagem(scanner);
+                break;
+
+                case 4:
+                    System.out.println("Encerrando o programa...");
+                break;
+
+                default:
+                    System.out.println("Opção inválida.");
+                    break;
             }
 
         } while (opcao != 4);
